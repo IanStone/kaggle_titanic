d1e103d7c777a1d85c7147737248c248362b4f92
Ian Stone <ian.stone@unh.edu>
2020-10-20 07:34:03 -0400

    Using title instead of age and sex

--- src/titanic.py
+++ src/titanic.py
@@ -17,3 +17,21 @@ def sex_numeric(df_sex, sex_col = 'Sex'):
     df_sex.loc[df_sex[sex_col].str.lower().str.strip() == 'female', 'Sex'] = 1
     df_sex[sex_col] = pd.to_numeric(df_sex[sex_col])
     return df_sex
+
+def group_by_title(df_names, title_col = 'Title'):
+    group_1 = ['the countess', 'mlle', 'sir', 'ms', 'lady', 'mme', 'mrs', 'miss']
+    group_3 = ['mr', 'jonkheer', 'rev', 'don', 'capt']
+
+    df_names.loc[df_names[title_col].isin(group_1), 'Title Group'] = 1
+    df_names.loc[df_names[title_col].isin(group_3), 'Title Group'] = 3
+    df_names.loc[df_names['Title Group'].isnull(), 'Title Group'] = 1
+
+    return df_names
+
+def has_family_onboard(df, family_counts = ['SibSp', 'Parch']):
+    for ea in family_counts:
+        df.loc[df[ea] > 0, 'Family Onboard'] = 1
+
+    df.loc[df['Family Onboard'].isnull(), 'Family Onboard'] = 0
+
+    return df
